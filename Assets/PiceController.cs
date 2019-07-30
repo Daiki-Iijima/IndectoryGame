a40700b80b3430b576eddfb3eb3dc8731086ea75
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-07-30 23:48:44 +0900

    Prefabの選択機能をつけた

---
+++ Assets/PiceController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiceController : MonoBehaviour
+{
+    public void Selected()
+    {
+        var defoMaterial = this.gameObject.GetComponent<MeshRenderer>().material;
+        var shader = defoMaterial.shader;
+        this.gameObject.GetComponent<MeshRenderer>().material = new Material(shader);
+        this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 1, 1);
+
+    }
+}
--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -17,6 +17,8 @@ public class ObjectManager : MonoBehaviour
     [SerializeField]
     private GameObject scrollContaint;
 
+    [SerializeField]
+    private GameObject picePrefab;
     public List<GameObject> gameObjects;
 
     // Start is called before the first frame update
@@ -36,6 +38,20 @@ public class ObjectManager : MonoBehaviour
             obj.GetComponent<ScrollViewContent>().SetInfoData(data);
             obj.transform.SetParent(scrollContaint.transform, false);
         });
+
+        //  フィールドの生成
+        for (int countX = -5; countX <= 5; countX++)
+        {
+            for (int countY = -5; countY <= 5; countY++)
+            {
+                var obj = Instantiate(picePrefab, new Vector3(countX, 0, countY), Quaternion.identity);
+
+                if (countX == 0)
+                {
+                    obj.GetComponent<PiceController>().Selected();
+                }
+            }
+        }
     }
 
 
@@ -46,6 +62,23 @@ public class ObjectManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 touchScreenPosition = Input.mousePosition;
+
+        touchScreenPosition.x = Mathf.Clamp(touchScreenPosition.x, 0.0f, Screen.width);
+        touchScreenPosition.y = Mathf.Clamp(touchScreenPosition.y, 0.0f, Screen.height);
+
+        Camera gameCamera = Camera.main;
+        Ray touchPointToRay = gameCamera.ScreenPointToRay(touchScreenPosition);
+
+        RaycastHit hitInfo = new RaycastHit();
+        if (Physics.Raycast(touchPointToRay, out hitInfo))
+        {
+            if (Input.GetMouseButton(0))
+                hitInfo.collider.gameObject.GetComponent<PiceController>().Selected();
+        }
+
+        // デバッグ機能を利用して、スクリーンビューでレイが出ているか見てみよう。
+        Debug.DrawRay(touchPointToRay.origin, touchPointToRay.direction * 1000.0f);
 
     }
 }
